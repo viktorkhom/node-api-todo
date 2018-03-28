fb9c8265b8a2321bc95f36369392e7b2b48c2647
viktor_khom <v.homenko@weget.nl>
2018-03-29 00:52:18 +0300

    added token

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,67 @@
 var mongoose  = require('mongoose');
+const validator = require('validator');
+var jwt = require('jsonwebtoken'); 
+const _ = reuire('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
 	email: {
 		type:String,
 		required:true,
 		minglength:1,
-		trim:true
-	}
+		trim:true,
+		unique:true,
+		validate: {
+			validator:validator.isEmail,
+			message:'{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type:String,
+		require:true,
+		minLength:6
+	},
+	tokens:[{
+		access: {
+			type:String,
+			require:true
+		},
+		token: {
+			type:String,
+			require:true
+		}
+	}]
 });
+
+UserSchema.methods.toJSON = function() {
+
+	var user = this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+
+};
+
+// register own method
+
+UserSchema.methods.generateAuthToken = function() {
+
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id:user._id.toHexString(), access}, 'abc123').toString();
+
+	user.tokens.push({
+		access,
+		token
+	});
+
+	return user.save().then((user) => {
+		
+		return token;
+	
+	});
+
+};
+
+var User = mongoose.model('User', UserSchema)
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -83,6 +83,8 @@ app.delete('/todos/:id', (req, res) => {
 		});
 });
 
+// update
+
 app.patch('/todos/:id', (req, res) => {
 
 	var id = req.params.id;
@@ -124,6 +126,24 @@ app.patch('/todos/:id', (req, res) => {
 
 	    });
 
+});
+
+
+// security and auth requests
+
+app.post('/users', (req, res) => {
+
+	var body = _.pick(req.body, ['email', 'password']);
+
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
 });
 
 app.listen(3000, () => {
