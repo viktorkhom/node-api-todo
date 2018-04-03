6926eec30fee09b50d8a49bcb362fc47e3936a1d
viktor_khom <v.homenko@weget.nl>
2018-04-03 20:32:13 +0300

    associate users with todos

--- server/models/user.js
+++ server/models/user.js
@@ -64,6 +64,22 @@ UserSchema.methods.generateAuthToken = function() {
 
 };
 
+UserSchema.methods.removeToken = function(token) {
+
+	var user = this;
+
+	return user.update({
+
+		$pull: {
+			tokens: {
+				token:token
+			}
+		}
+
+	});
+
+};
+
 UserSchema.statics.findByToken = function(token) {
 
 	var User = this;
@@ -86,6 +102,36 @@ UserSchema.statics.findByToken = function(token) {
 		"tokens.access":"auth"
 
 	});
+};
+
+UserSchema.statics.findByCredentials = function(email,password) {
+
+	var User = this;
+
+	return User.findOne({email}).then((user) => {
+
+		if(!user) {
+			return Promise.reject();
+		}
+
+		return new Promise((resolve, reject) => {
+
+			bcrypt.compare(password, user.password, (err, res) => {
+				
+				if(res) {
+					resolve(user);
+				}
+				
+				else {
+					reject();
+				}
+				
+			});
+
+		});
+
+	});
+
 };
 
 // executed some action before some action)
--- server/server.js
+++ server/server.js
@@ -12,11 +12,12 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
 
 	var todo = new Todo({
 
-		text:req.body.text
+		text:req.body.text,
+		_creator:req.user._id
 
 	});
 
@@ -27,9 +28,11 @@ app.post('/todos', (req, res) => {
 	});
 });
 
-app.get('/todos', (req, res) => {
+app.get('/todos', authenticate, (req, res) => {
 	
-	Todo.find().then((todos) => {
+	Todo.find({
+		_creator:req.user._id
+	}).then((todos) => {
 		res.send({
 			todos
 		});
@@ -147,9 +150,42 @@ app.post('/users', (req, res) => {
 	});
 });
 
+app.post('/users/login', (req, res) => {
+
+	var body = _.pick(req.body, ['email', 'password']);
+
+	user.findByCredentials(body.email, body.password)
+	.then((user) => {
+
+		return user.generateAuthToken().then((token) => {
+
+			res.header('x-auth', token).send(user);
+
+		});
+
+	}).catch((e) => {
+
+		res.status(400).send();
+
+	});
+
+});
+
 app.get('/users/me', authenticate, (req,res) => {
 
 	res.send(req.user);
+
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+
+	req.user.removeToken(req.token).then(() => {
+
+		res.status(200).send();
+
+	}, () => {
+		res.status(400).send();
+	});
 
 });
 
